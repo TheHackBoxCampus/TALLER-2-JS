d35df7d5d1ae1a6099b58a1022a515b2dc416bc1
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-03-09 12:56:51 -0500

    feat: ✨ Ejercicio 2
    ##Dado un número indicar si es par o impar y si es mayor de 10.

--- Main.js
+++ Main.js
@@ -1,19 +1,7 @@
-let x = 0; 
-let notes = []; 
+let number = parseInt(prompt("Ingrese el numero"))
+number % 2 == 0 ? console.log("Par") : console.log("Impar")
+number < 10 ? console.log("Menor que Diez"):console.log("Mayor o igual que 10") 
 
-const average_calc = (arr, cont) => {
-    while(cont < 3){
-        let note = parseInt(prompt("Ingrese la nota: "))
-        if (!note) return;
-        arr.push(note)
-        cont+=1
-    }
 
-    let average = arr.reduce((ac, it) => (ac + it))
-    return (average / 3) <= 3.9 ? "Estudie, mire su promedio " + (average / 3) : "Becado, excelente" 
-}
-
-window.alert("\t La calificacion se tomara en formato de 1 a 10, contando decimales")
-console.log(average_calc(notes, x))
 
 
