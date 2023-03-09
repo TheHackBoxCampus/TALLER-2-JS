a70aa94cff04bb64fd6ed403f32661a41c8ea712
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-03-09 13:15:20 -0500

    feat: ✨ Ejercicio 3
    ##Construir el algoritmo para determinar el voltaje de un
    circuito a partir de la resistencia y la intensidad de corriente.

--- Main.js
+++ Main.js
@@ -1,7 +1,14 @@
-let number = parseInt(prompt("Ingrese el numero"))
-number % 2 == 0 ? console.log("Par") : console.log("Impar")
-number < 10 ? console.log("Menor que Diez"):console.log("Mayor o igual que 10") 
+//Ley de Ohm 
+let voltage_end; 
+let Intesidad = parseInt(prompt("Ingrese la intesidad de corriente: "))
+let Resistencia = parseInt(prompt("Ingrese la Resistencia: "))
 
 
+const voltage = (I, R, V) => {
+    V = I * R; 
+    console.log(`La cantidad de voltaje que se genero fue: \n   
+    V = ${I}A x ${R}Ω = ${V}V`)
 
+}; 
 
+voltage(Intesidad, Resistencia, voltage_end)
