a67e71258dd5b36f77762c5e18907bd08f3fba8b
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-03-10 07:16:28 -0500

    feat: ✨ Ejercicio 9
    ##N atletas han pasado a finales en salto triple en los juegos
    olímpicos femenino de 2022. Diseñe un programa que pida por
    teclado los nombres de cada atleta finalista y a su vez, sus
    marcas del salto en metros. Informar el nombre de la atleta
    campeona que se quede con la medalla de oro y si rompió
    récord, reportar el pago que será de 500 millones. El récord
    esta en 15,50 metros.

--- Main.js
+++ Main.js
@@ -1,12 +1,27 @@
-let side = parseInt(prompt("\tIngrese el lado del cuadrado para calcular el perimetro")); 
-let base = parseInt(prompt("\tIngrese la base del rectangulo"));
-let height = parseInt(prompt("\tIngrese la altura del rectangulo"));
+let finalists = []
+let position = []
+let amound = parseInt(prompt("Ingrese el numero de finalistas:")); 
+let options = ["Finalista: ", "metros"]; 
+let y = 0; 
 
+while(y < amound){
+    for(let x = 0; x < options.length; x++){
+        let name = prompt(options[0]);
+        let meters = parseInt(prompt(options[1]));  
+        finalists.push({name, meters})
+        break; 
+    }
+    y+=1; 
+}
 
-window.alert(`El perimetro del cuadrado es ${side * 4}`)
-window.alert(`La area del cuadrado es:  ${base * height}`)
-
-
-
-
-
+if(finalists.length >= 1){
+    let max_finalist  = finalists.sort((a,b) => b.name - a.name);
+    max_finalist[max_finalist.length - 1].meters > 15.50 
+    ? window.alert(`Rompio record, gano 500 millones el finalista ${max_finalist[max_finalist.length - 1].name} 
+    con un puntaje de ${max_finalist[max_finalist.length - 1].meters}`) 
+    : window.alert(`El que recorrio mas metros pero no rompio el record fue, 
+    ${max_finalist[max_finalist.length - 1].name} con 
+    ${max_finalist[max_finalist.length - 1].meters} metros recorridos`)
+}else {
+    console.log("vacio")
+}
