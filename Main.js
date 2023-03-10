33d6317ab55e23ba6ec8447085170e69a5e70897
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-09 19:28:40 -0500

    feat: ✨ Ejercicio 6
    ##Construir el algoritmo en Javascript para un programa
    para cualquier cantidad de estudiantes que lea el nombre,
    el sexo y la nota definitiva y halle al estudiante con la mayor
    nota y al estudiante con la menor nota y cuantos eran
    hombres y cuantos mujeres.

--- Main.js
+++ Main.js
@@ -1,8 +1,40 @@
-let num1 = parseInt(prompt("Ingrese el primero numero: "))
-let num2 = parseInt(prompt("Ingrese el segundo numero: "))
+let amount = parseInt(prompt("Ingrese la cantidad de estudiantes"))
+let x = 0;
+let M = 0; 
+let F = 0; 
+let save_and_options = {
+    M: "Masculino",
+    F: "Femenino",
+    notes: []
+};
 
-const op = (num, num2) => { 
-    if(num<num2) console.log(`La suma es ${num + num2}, y la diferencia es ${num - num2}`)
-    if(num>num2) console.log(`El producto es ${num * num2}, y la division es ${num / num2}`)  
+while(x < amount){
+    let name_student = prompt("\t Ingrese su nombre: ");
+    let sex = prompt("\t Ingrese su sexo: \n\t M -- F");
+    let note_definitive = parseInt(prompt("\t Ingrese su nota definita: "));
+    if(save_and_options[sex.toUpperCase()] == undefined) {
+        x = amount;
+        window.alert(`Datos Incorrectos de ${name_student}`)
+    } 
+    else{
+      save_and_options[sex.toUpperCase()] == "Masculino" ? M++ : F++;
+      save_and_options.notes.push({name: name_student, note: note_definitive});
+    }   
+    x+=1;
 }
-op(num1, num2)
+const order = save_and_options.notes.sort((a,b) => a.note - b.note); 
+
+// execute
+console.log(`
+ ---------------------------------------------
+  \tEl Estudiante con mayor nota fue ${order[order.length - 1].name},
+  \tcon una nota de ${order[order.length - 1].note}
+  \tEl estudiante con menor nota fue ${order[0].name}
+  \tcon una nota de ${order[0].note},
+  \tMujeres en total: ${F}
+  \tHombres en total: ${M}
+ ---------------------------------------------
+`);
+
+
+
