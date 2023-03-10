6ec43f3522ea3dd3fd5a65b3b7387f951fe76382
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-09 20:25:52 -0500

    feat: ✨ Ejercicio 7
    ## Programa que pida el ingreso del nombre y precio de un artículo y la
    cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador
    en su factura.

--- Main.js
+++ Main.js
@@ -1,40 +1,25 @@
-let amount = parseInt(prompt("Ingrese la cantidad de estudiantes"))
-let x = 0;
-let M = 0; 
-let F = 0; 
-let save_and_options = {
-    M: "Masculino",
-    F: "Femenino",
-    notes: []
-};
+let buys = parseInt(prompt("\tIngrese el numero de productos que quiere comprar")); 
+let amount_client = parseFloat(prompt("\tIngrese su cantidad de dinero"))
+let x = 0; 
+let total_products = [];
+let total_price = [];
 
-while(x < amount){
-    let name_student = prompt("\t Ingrese su nombre: ");
-    let sex = prompt("\t Ingrese su sexo: \n\t M -- F");
-    let note_definitive = parseInt(prompt("\t Ingrese su nota definita: "));
-    if(save_and_options[sex.toUpperCase()] == undefined) {
-        x = amount;
-        window.alert(`Datos Incorrectos de ${name_student}`)
-    } 
-    else{
-      save_and_options[sex.toUpperCase()] == "Masculino" ? M++ : F++;
-      save_and_options.notes.push({name: name_student, note: note_definitive});
-    }   
-    x+=1;
-}
-const order = save_and_options.notes.sort((a,b) => a.note - b.note); 
+while(x < buys) {
+    let name_product = prompt("\tIngrese el nombre del producto:");
+    let price = parseInt(prompt("\tIngrese el precio:"));
+    total_products.push(name_product) 
+    total_price.push(price)
+    x+=1; 
+}   
 
-// execute
+let products = total_products.map(product => product)
+let total_buy = total_price.reduce((ac,itm) => ac + itm);
+
 console.log(`
- ---------------------------------------------
-  \tEl Estudiante con mayor nota fue ${order[order.length - 1].name},
-  \tcon una nota de ${order[order.length - 1].note}
-  \tEl estudiante con menor nota fue ${order[0].name}
-  \tcon una nota de ${order[0].note},
-  \tMujeres en total: ${F}
-  \tHombres en total: ${M}
- ---------------------------------------------
-`);
-
-
-
+---------------------------------
+Producto/s: ${products}
+cantidad a pagar: ${total_buy}
+cantidad del cliente: ${amount_client}
+vueltos: ${amount_client - total_buy}
+---------------------------------
+`)
