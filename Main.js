86c5726ef087ee3cb98c8f2f8f46b1456447cff3
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-03-10 08:14:41 -0500

    feat: ✨ Ejercicio 10
    Desarrolle un programa cíclico que capture un dato
    numérico cada vez, y los vaya acumulando. El programa se
    detiene cuando el usuario digita un cero. El programa debe
    mostrar: LA SUMATORIA DE LOS VALORES, EL VALOR DEL
    PROMEDIO, CUÁNTOS VALORES FUERON DIGITADOS, MAYOR
    VALOR Y MENOR VALOR.

--- Main.js
+++ Main.js
@@ -1,27 +1,24 @@
-let finalists = []
-let position = []
-let amound = parseInt(prompt("Ingrese el numero de finalistas:")); 
-let options = ["Finalista: ", "metros"]; 
-let y = 0; 
+let numerics = []
+let entered_values = 0;  
 
-while(y < amound){
-    for(let x = 0; x < options.length; x++){
-        let name = prompt(options[0]);
-        let meters = parseInt(prompt(options[1]));  
-        finalists.push({name, meters})
-        break; 
-    }
-    y+=1; 
+while(true){
+  let numeric = parseInt(prompt("Ingrese un dato numerico: ")); 
+  if (numeric == 0 || !numeric) break; 
+  numerics.push(numeric)
+  entered_values++; 
 }
 
-if(finalists.length >= 1){
-    let max_finalist  = finalists.sort((a,b) => b.name - a.name);
-    max_finalist[max_finalist.length - 1].meters > 15.50 
-    ? window.alert(`Rompio record, gano 500 millones el finalista ${max_finalist[max_finalist.length - 1].name} 
-    con un puntaje de ${max_finalist[max_finalist.length - 1].meters}`) 
-    : window.alert(`El que recorrio mas metros pero no rompio el record fue, 
-    ${max_finalist[max_finalist.length - 1].name} con 
-    ${max_finalist[max_finalist.length - 1].meters} metros recorridos`)
-}else {
-    console.log("vacio")
-}
+if(numerics.length >= 1){
+    let summation = numerics.reduce((ac, itm) => ac + itm); 
+    let average = summation / entered_values; 
+    let max_number = Math.max(...numerics);
+    let min_number = Math.min(...numerics); 
+
+    window.alert(`
+    \t La sumatoria fue ${summation} , 
+    \t promedio de ${average},  
+    \t el total de valores digitados fue ${entered_values}, 
+    \t el numero maximo ${max_number}
+    \t el numero menor ${min_number}`
+    )
+}else console.log("vacio")
