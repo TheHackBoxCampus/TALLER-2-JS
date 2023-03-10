e955aa15cd4ae15cc733a9d6f806b5efbccb689e
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-03-10 06:07:49 -0500

    feat: ✨ Ejercicio 8
    ## Programa que Ingrese por teclado:
    a. el valor del lado de un cuadrado para mostrar por pantalla el
    perímetro del mismo
    b. la base y la altura de un rectángulo para mostrar el área del
    mismo

--- Main.js
+++ Main.js
@@ -1,25 +1,12 @@
-let buys = parseInt(prompt("\tIngrese el numero de productos que quiere comprar")); 
-let amount_client = parseFloat(prompt("\tIngrese su cantidad de dinero"))
-let x = 0; 
-let total_products = [];
-let total_price = [];
+let side = parseInt(prompt("\tIngrese el lado del cuadrado para calcular el perimetro")); 
+let base = parseInt(prompt("\tIngrese la base del rectangulo"));
+let height = parseInt(prompt("\tIngrese la altura del rectangulo"));
 
-while(x < buys) {
-    let name_product = prompt("\tIngrese el nombre del producto:");
-    let price = parseInt(prompt("\tIngrese el precio:"));
-    total_products.push(name_product) 
-    total_price.push(price)
-    x+=1; 
-}   
 
-let products = total_products.map(product => product)
-let total_buy = total_price.reduce((ac,itm) => ac + itm);
+window.alert(`El perimetro del cuadrado es ${side * 4}`)
+window.alert(`La area del cuadrado es:  ${base * height}`)
 
-console.log(`
----------------------------------
-Producto/s: ${products}
-cantidad a pagar: ${total_buy}
-cantidad del cliente: ${amount_client}
-vueltos: ${amount_client - total_buy}
----------------------------------
-`)
+
+
+
+
